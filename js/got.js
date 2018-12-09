353576f6aa1401bd9f0a4883b0a93494f8d15a2c
Renáta Csorba <csorba.renata@gmail.com>
2018-12-09 13:00:36 +0100

    created basic html-layout with css and js by sorted and displayed alive characters on portrait-div, and added fonts

--- js/got.js
+++ js/got.js
@@ -13,6 +13,9 @@ function successGetGameOfThronesCharacterDatas(xhttp) {
   // Nem szabad globálisba kitenni a userDatas-t!
   var userDatas = JSON.parse(xhttp.responseText);
   // Innen hívhatod meg a többi függvényed
+  var charactersAlive = findAliveCharacters(userDatas);
+  sortAliveCharacters(charactersAlive);
+  showCharacterList(charactersAlive);
 }
 
 getGameOfThronesCharacterDatas(
@@ -22,3 +25,34 @@ getGameOfThronesCharacterDatas(
 
 // Live servert használd mindig!!!!!
 /* IDE ÍRD A FÜGGVÉNYEKET!!!!!! NE EBBE AZ EGY SORBA HANEM INNEN LEFELÉ! */
+
+function findAliveCharacters(userDatas) {
+  var alivePeople = [];
+  for (var i = 0; i < userDatas.length; i++) {
+    if (!userDatas[i].dead) {
+      alivePeople.push(userDatas[i]);
+    }
+  }
+  return alivePeople;
+}
+
+function sortAliveCharacters(charactersAlive) {
+  charactersAlive.sort(function sortByNames(a, b) {
+    if (a.name > b.name) {
+      return 1;
+    }
+    return -1;
+  });
+}
+
+function showCharacterList(charactersAlive) {
+  var characterTable = document.querySelector('#portraits');
+  var characterRows = '';
+  sortAliveCharacters(charactersAlive);
+  for (var i = 0; i < charactersAlive.length; i++) {
+    characterRows += `
+      <div class="portraits__div"><img src="${charactersAlive[i].portrait}" alt="${charactersAlive[i].name}"<br><br>${charactersAlive[i].name}<br></div>
+      `;
+  }
+  characterTable.innerHTML = characterRows;
+}
